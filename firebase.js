ca896aa4d893a558cf6f2e7fdf244910bb450eba
Anjul Singhal <anjulsinghal123@gmail.com>
2024-10-14 02:23:03 +0530

    first commit

---
+++ firebase.js
@@ -0,0 +1,20 @@
+// // Import the functions you need from the SDKs you need
+// import { initializeApp } from "firebase/app";
+// // TODO: Add SDKs for Firebase products that you want to use
+// // https://firebase.google.com/docs/web/setup#available-libraries
+
+// // Your web app's Firebase configuration
+// const firebaseConfig = {
+//   apiKey: "AIzaSyAeizN0rNEQESvcZaJg53qujOmQFBhtVKI",
+//   authDomain: "react-jobs-9b4e3.firebaseapp.com",
+//   projectId: "react-jobs-9b4e3",
+//   storageBucket: "react-jobs-9b4e3.appspot.com",
+//   messagingSenderId: "481767615096",
+//   appId: "1:481767615096:web:2771e3c6a91238c594db8d"
+// };
+
+// // Initialize Firebase
+// const app = initializeApp(firebaseConfig);
+
+// export const auth = getAuth(app)
+// export const db = getFirestore(app)
